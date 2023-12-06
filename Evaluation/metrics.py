24f603beb520341e4687f619a7f2f0f9734bcf95
LucaBrembilla <lucabremby00@gmail.com>
2023-12-06 10:37:23 +0100

    Update np.float

--- Evaluation/metrics.py
+++ Evaluation/metrics.py
@@ -290,7 +290,7 @@ class _Global_Item_Distribution_Counter(_Metrics_Object):
     def __init__(self, n_items, ignore_items):
         super(_Global_Item_Distribution_Counter, self).__init__()
 
-        self.recommended_counter = np.zeros(n_items, dtype=np.float)
+        self.recommended_counter = np.zeros(n_items, dtype=np.float64)
         self.ignore_items = ignore_items.astype(np.int).copy()
 
 
